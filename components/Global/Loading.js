f3e8badc1d6115370a1495dd0056d0fa83e8691f
Robert Henderson <rhahenderson@gmail.com>
2021-06-04 18:23:48 -0700

    Added Loading svg to Loader cmp, removed onReady handler from Datepicker

--- components/Global/Loading.js
+++ components/Global/Loading.js
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+
 const Loading = () => {
     return (
-        <>
-            <div>Loading...</div>
-        </>
+        <div className="w-full h-full fixed block top-0 left-0 bg-white opacity-75 z-50">
+            <span className="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0">
+                <img
+                    className="transform -translate-x-1/2 -translate-y-1/2 max-w-none"
+                    src="/rings.svg"
+                    alt="Loading..."
+                    style={{ width: 200 + 'px' }}
+                />
+            </span>
+        </div>
     );
 };
 
